962374677a542c37f17ad2b840a1108e3a7c57b9
siljee <silje.hansen3@nav.no>
2020-09-23 00:41:07 +0200

    Add header and dummy-card

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-2
+# Class vs functions
+
+Recommended to use functions and much easier (more readable, less code). 
+But classes can be more flexible. And used in old apps before React Hooks (functions did not have state.)
+Thus, this app is created with Classes. 
+
+## Class
+
+**Render-function**
+
+A React Class-component must have a render function. 
+It returnes the viritual DOM description of your component. 
+
+**This**
+
+Props is accessed from `this`: 
+```js
+{this.props.title}
+```
+
+**Constructor**
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="header">GitHub Profiles</div>
+        <Card />
+      </>
+    )
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="Company">Company here...</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
